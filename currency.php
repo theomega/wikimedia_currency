2d2bebc773d27d54c2b23ffa3d26bf195222fffd
Dominik Bruhn <dominik@dbruhn.de>
2013-01-21 00:25:25 +0100

    PHP: Renamed function to reflect to/from USD.

--- currency.php
+++ currency.php
@@ -22,13 +22,9 @@ class CurrencyConverter {
     #Delete data from mysql-table
     $this->query("TRUNCATE TABLE exchange_rates;");
 
-    $stmt = $this->mysql->prepare("INSERT INTO exchange_rates ".
+    $stmt = $this->prepare("INSERT INTO exchange_rates ".
                                   "(currency, rate) VALUES (?,?);");
      
-    if($stmt==FALSE) {
-      die('MySQL prepare has failed: '.$this->mysql->error);
-    }
-
     #Iterate on all rates and insert them into the table     
     $count=0;
 
@@ -37,8 +33,57 @@ class CurrencyConverter {
       $stmt->execute();
       $count++;
     } 
+    
+    $stmt->close();
 
     return $count;
+  }
+
+
+  #Converts a foreign currency (in the format "JPY 3254") into the equivalent in 
+  #USD (in the format "USD 6345")
+  #
+  #This function has a strange signature (as requested) based on a string as
+  #and a string as output (which should be formated). It takes either an array
+  #of strings or an single string.
+  public function convertToUSDFromStr($s) {
+    if(is_array($s)) {
+      return array_map(array($this, 'convertToUSDFromStr'), $s);
+    } else {
+      $a = explode(" ", $s);
+      if(count($a)!=2) {
+        die('Invalid parameter format "'.$s.'"');
+      }
+      return 'USD '.sprintf('%01.2f', $this->convertToUSD($a[0], $a[1]));
+    }
+  }
+
+  #Converts a foreign currency into the equivalent in USD
+  #
+  #This is a function which has a better suited signature, based on two
+  #parameters and returning a single double, which is not truncated.
+  public function convertToUSD($currency, $amount) {
+    return $amount*$this->getRate($currency);
+  }
+
+  #Converts a amount of USD into the equivalent in a foreign currency
+  public function convertFromUSD($amount, $currency) {
+    return $amount/$this->getRate($currency);
+  }
+
+  #Helper function which simply returns the exchange rate for a give currency
+  private function getRate($currency) {
+    $stmt = $this->prepare("SELECT rate FROM exchange_rates WHERE ".
+                           "currency=?;");
+    $stmt->bind_param('s', $currency);
+    $stmt->execute();
+    $stmt->bind_result($rate);
+    if($stmt->fetch()!=TRUE) {
+      die("Could not get rate for currency '".$currency."'");
+    }
+    $stmt->close();
+
+    return $rate;
   }
 
   #Wrapper function on mysqli::query which checks for errors and dies if an
@@ -50,6 +95,18 @@ class CurrencyConverter {
     }
     return $r;
   }
+
+  #Wrapper function on mysqli::prepare which checks for errors and dies in case
+  #of an error
+  private function prepare($query) {
+    $s = $this->mysql->prepare($query);
+    if($s==FALSE) {
+      die('MySQL-Statement prepare "'.$query.'" failed: '.$this->mysql->error);
+    }
+    return $s;
+  }
+
+
 
   #Wrapper function on libxmls loading to check for errors and die in case of an
   #error
@@ -66,17 +123,33 @@ class CurrencyConverter {
   }
 }
 
+$cc=new CurrencyConverter('localhost', 'wikimedia', 'wikimedia', 'wikimedia');
+
 if(!isset($_GET['action'])) {
   die('Specify the "action" GET parameter');
 }
 
 $action=$_GET['action'];
 
+
 if($action=='refresh') {
   echo('Refreshing the conversion data<br />');
-  $cc=new CurrencyConverter('localhost', 'wikimedia', 'wikimedia', 'wikimedia');
   $count = $cc->refresh();
   echo('Inserted '.$count.' records in the database');
+} else if ($action=='fromUSD') {
+  if(!isset($_GET['currency'])) {
+    die('Specify the "currency" parameter');
+  }
+  if(!isset($_GET['amount'])) {
+    die('Specify the "amount" parameter');
+  }
+  echo $cc->convertFromUSD($_GET['amount'], $_GET['currency']);
+} else if ($action=='test') {
+  echo 'Single Test: <br/>';
+  echo 'JPY 5000: '.$cc->convertToUSDFromStr('JPY 5000').'<br/>';
+  echo 'Multiple Test: <br/>';
+  echo 'array(JPY 5000, CZK 62.5): ';
+  print_r($cc->convertToUSDFromStr(array( 'JPY 5000', 'CZK 62.5')));
 }
 
 ?>
